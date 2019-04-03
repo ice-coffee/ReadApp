d1b8a493ee259e05e6452231309acf3409eb03d3
miaozepeng <2677344716@qq.com>
2019-04-03 11:41:25 +0800

    1. 书架部分UI

--- src/pages/bookcase/index.vue
+++ src/pages/bookcase/index.vue
@@ -1,7 +1,57 @@
 <template>
-  <span>书架</span>
+  <div class="bookcase">
+
+    <!--搜索-->
+    <div class="home_search">
+      <div>
+        <img class="icon" src="/static/images/ic_search.png"/>
+      </div>
+      <div class="input">
+        <input type="text" placeholder="书名/作者"/>
+      </div>
+    </div>
+
+    <!--轮播图-->
+    <div class="home_banner">
+      <swiper autoplay="true" interval="3000" duration="500" circular="true">
+        <block v-for="(item, index) in banner" :key="index">
+          <swiper-item>
+            <img :src="item.bannerImage" class="slide-image" />
+          </swiper-item>
+        </block>
+      </swiper>
+    </div>
+
+    <div class="home_icon">
+      <div class="icon_rank">
+
+      </div>
+      <div class="icon_classification">
+
+      </div>
+      <div class="icon_">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data () {
+    return {
+      banner: [
+        {bannerImage: 'http://i0.hdslb.com/bfs/archive/ffe9735cdb517513b7de05d95767eef31abe3da9.jpg'},
+        {bannerImage: 'http://i0.hdslb.com/bfs/archive/ffe9735cdb517513b7de05d95767eef31abe3da9.jpg'},
+        {bannerImage: 'http://i0.hdslb.com/bfs/archive/ffe9735cdb517513b7de05d95767eef31abe3da9.jpg'},
+        {bannerImage: 'http://i0.hdslb.com/bfs/archive/ffe9735cdb517513b7de05d95767eef31abe3da9.jpg'}
+      ]
+    }
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+  @import "./style.scss";
+</style>
+
